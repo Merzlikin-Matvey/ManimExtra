29ca3797ad5a401b7a28dd7a808eb39649aa5f4a
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2025-01-15 18:26:29 +0300

    Минор

--- docs/conf.py
+++ docs/conf.py
@@ -23,12 +23,6 @@ intersphinx_mapping = {
 }
 
 html_theme_options = {
-    "source_repository": "https://github.com/ManimCommunity/manim/",
-    "source_branch": "main",
-    "source_directory": "docs/source/",
-    "top_of_page_button": None,
-    "light_logo": "manim-logo-sidebar.svg",
-    "dark_logo": "manim-logo-sidebar-dark.svg",
     "light_css_variables": {
         "color-content-foreground": "#000000",
         "color-background-primary": "#ffffff",
