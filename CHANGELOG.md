912584211d910206a79badac613aad2c5a55ea96
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2025-04-21 12:52:18 +0300

    Refactor triangle center classes to inherit from TriangleCenter

---
+++ CHANGELOG.md
@@ -0,0 +1,192 @@
+# Changelog
+
+All significant changes to the project are recorded in this file
+
+---
+
+## [1.12] — 2025.04.01
+
+## Added
+- Added Sphinx documentation with deployment on GitHub pages
+- Added CI/CD pipeline for deployment to GitHub Releases
+- Added `utils\docbuild` module for building documentation
+
+## Changed
+- Python 3.13 is now supported
+- Updated docstrings in the code for Sphinx documentation
+- Development dependencies removed from `pyproject.toml`
+- `Point3DLike` is now used
+
+## Fixed
+- Fixed `from_three_points` method in `Angle` class
+
+## [1.11] — 2024.09.14
+
+## Added
+- Added CI/CD pipline for automatic deployment to PyPI
+- Added flake8 linter to the project (also in CI/CD)
+- Added `change_right_point` method to the `UnitCircle` class for changing the right point of the unit circle 
+(for example, from 0 to 2π)
+- Added `NinePoint Circle` class, which represents the nine-point circle of a triangle
+
+## Changed
+- Adapted the code to the style set by linter
+
+## Fixed
+- Small fixes in the `UnitCircle` class
+- Fixed `TransformSystem` class`
+
+## [1.10] — 2024.08.13
+
+## Added
+- Added `BracketBetweenPoints` class for creating brackets between two points
+- Added `UnitCircle` class for creating a unit circle for trigonometry
+- Added methods for `UnitCircle` class:
+  - `get_center`: returns the center coordinates of the unit circle.
+  - `add_point`: adds a new point on the circle at a given angle and attaches a label to it.
+  - `show_labels`: displays the default labels (right, up, left, and down) on the circle.
+  - `hide_labels`: hides the currently visible labels.
+  - `show_horizontal`: adds the horizontal axis (line) to the unit circle.
+  - `hide_horizontal`: removes the horizontal axis from the unit circle.
+  - `show_vertical`: adds the vertical axis (line) to the unit circle.
+  - `hide_vertical`: removes the vertical axis from the unit circle.
+  - `show_axes`: displays both horizontal and vertical axes simultaneously.
+  - `hide_axes`: hides both horizontal and vertical axes simultaneously.
+  - `get_arc`: returns an arc segment on the circle between two specified angles.
+- Added `SwapEquations` animation class for swapping equations in a system of equations
+- Added `swap` method to `SystemOfEquations` class for swapping equations in a system of equations
+
+## [1.9] — 2024.07.31
+
+## Added
+
+- Added `Bracket` class for creating brackets. Similar to `Brace`
+- Added `SystemOfEquations` class for creating systems of equations. You can also create sets of equations
+  using the `is_bracket`
+- Added `TransformSystem` class for transforming systems of equations
+- Added `EuclidLine` class. This line is parallel to the side of the triangle and passes through the opposite vertex
+- Added `Circumcircle` class, which represents the circumcircle of a triangle
+- Added `CarnotCircle` class
+- Now the project is distributed under the MIT license
+
+## Changed
+
+- Simplified project dependencies
+
+## [1.8] — 2024.07.24
+
+## Added
+
+- Added `get_diametrically_opposite_point` method to `Circle` class
+
+## Changed
+
+- Changed `bisected_angles` method in `Bisector` class. Now angles are slightly different because of
+  the `radius_alpha=1.1` parameter
+
+## Fixed
+
+- Fixed `set_length_about_point` method in `Line` class. Now it works with `Dot` correctly
+- Fixed `Angle` and `Perpendicular` class initialization
+- Fixed `intersection_lines` function
+
+## [1.7] — 2024.07.24
+
+## Added
+
+- Added `Incircle` class, which represents the incircle of a triangle
+
+## [1.6] — 2024.07.20
+
+## Added
+
+- Added `Fancy_label` function for easy writing of a title with dynamic animation time
+- Added `Piechart` class for creating pie charts
+
+## Changed
+
+- Changed `get_label_center` method in `Angle` class
+
+## [1.5] — 2024.06.08
+
+## Added
+
+- Added `Tangent` class, which represents a tangent to a circle
+- Added `.gitignore` file to ignore unnecessary files
+
+## Changed
+
+- Changed `from_three_points` method in `Angle` class. Now the angle is always less than 180 degrees
+
+## Fixed
+
+- Fixed `__init__` method in `Angle` and `Dot`
+
+## [1.4] — 2024.02.06
+
+## Added
+
+- Added new triangle cevians and related classes:
+    - `Cevian`
+    - `Bisector`
+    - `Median`
+    - `Symmedian`
+    - `Perpendicular`
+    - `PerpendicularBisector`
+    - `Altitude`
+
+## [1.3] — 2024.02.06
+
+## Added
+
+- Added functions to get intersections of some curves:
+    - `intersection_lines`
+    - `intersection_circles`
+    - `intersection_line_and_circle`
+
+## [1.2] — 2024.02.06
+
+## Added
+
+- Added triangle centers:
+    - `Incenter`
+    - `Excenter`
+    - `Centroid`
+    - `Circumcenter`
+    - `Orthocenter`
+    - `NinePointCenter`
+    - `LemoinePoint`
+    - `GergonnePoint`
+    - `NagelPoint`
+    - `Mittenpunkt`
+    - `SpiekerCenter`
+    - `FeuerbachPoint`
+    - `FermatPoint`
+
+## Fixed
+
+- Fixed `from_three_points` method in `Angle`. Now you can transfer regular `Dot`
+
+## [1.1] — 2024.02.01
+
+### Added
+
+- Added the `inversion` method to the `Line` class, that returns the inversion of the line relative to given `Circle`
+
+## [1.0] — 2024-01-28
+
+### Added
+
+- Initial release of the project
+- Custom `Line`, `Angle`, and `Circle` classes. You can pass not only NumPy arrays to them, but also regular `Dot`
+- Added methods to the `Line` class:
+    - `is_point_in_line`: checks if the point lies on a straight line
+    - `set_length_about_point`: sets the length of a straight line, with one end remaining at a given point
+    - `get_distance`: gets the distance from a point to a straight line
+    - `equal`: adds a straight equality icon to the middle
+    - `paral`: adds a straight parallelism icon to the middle
+- Added the `get_label_center` method to the `Angle` class, which returns a point inside the corner where it is
+  convenient to insert a label
+- Added methods to the `Circle` class:
+    - `pow`: returns the power of a point relative to a circle
+    - `is_point_in_circle`: checks if the point lies inside a circle=
--- manimextra/geometry/triangle_centers.py
+++ manimextra/geometry/triangle_centers.py
@@ -1,6 +1,5 @@
 from .default import *
 
-
 __all__ = [
     "Incenter",
     "Excenter",
@@ -41,122 +40,129 @@ def csc(alpha) -> float:
     return 1 / np.sin(alpha)
 
 
-class Incenter(Dot):
+class TriangleCenter(Dot):
+    def __init__(self, A, B, C, trilinear_coordinates, **kwargs):
+        A, B, C = dot_to_array(A, B, C)
+        self.A = A
+        self.B = B
+        self.C = C
+        self.trilinear_coordinates = trilinear_coordinates
+        super().__init__(trilinear_to_cartesian(A, B, C, trilinear_coordinates), **kwargs)
+
+
+class Incenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
-        super().__init__(trilinear_to_cartesian(A, B, C, (1, 1, 1)), **kwargs)
+        super().__init__(A, B, C, (1, 1, 1), **kwargs)
 
 
-class Excenter(Dot):
+class Excenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
-        super().__init__(trilinear_to_cartesian(A, B, C, (1, -1, 1)), **kwargs)
+        super().__init__(A, B, C, (1, -1, 1), **kwargs)
 
 
-class Centroid(Dot):
+class Centroid(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (1 / a, 1 / b, 1 / c)), **kwargs)
+        super().__init__(A, B, C, (1 / a, 1 / b, 1 / c), **kwargs)
 
 
-class Circumcenter(Dot):
+class Circumcenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         angle_a = Angle.from_three_points(B, A, C).get_value()
         angle_b = Angle.from_three_points(A, B, C).get_value()
         angle_c = Angle.from_three_points(B, C, A).get_value()
-        super().__init__(trilinear_to_cartesian(A, B, C, (np.cos(angle_a), np.cos(angle_b), np.cos(angle_c))), **kwargs)
+        super().__init__(A, B, C, (np.cos(angle_a), np.cos(angle_b), np.cos(angle_c)), **kwargs)
 
 
-class Orthocenter(Dot):
+class Orthocenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         angle_a = Angle.from_three_points(B, A, C).get_value()
         angle_b = Angle.from_three_points(A, B, C).get_value()
         angle_c = Angle.from_three_points(B, C, A).get_value()
-        super().__init__(trilinear_to_cartesian(A, B, C, (sec(angle_a), sec(angle_b), sec(angle_c))), **kwargs)
+        super().__init__(A, B, C, (sec(angle_a), sec(angle_b), sec(angle_c)), **kwargs)
 
 
-class NinePointCenter(Dot):
+class NinePointCenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         angle_a = Angle.from_three_points(B, A, C).get_value()
         angle_b = Angle.from_three_points(A, B, C).get_value()
         angle_c = Angle.from_three_points(B, C, A).get_value()
-        super().__init__(trilinear_to_cartesian(
-            A, B, C, (np.cos(angle_b - angle_c), np.cos(angle_c - angle_a), np.cos(angle_a - angle_b))), **kwargs)
+        super().__init__(A, B, C, (np.cos(angle_b - angle_c), np.cos(angle_c - angle_a), np.cos(angle_a - angle_b)), **kwargs)
 
 
-class LemoinePoint(Dot):
+class LemoinePoint(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (a, b, c)), **kwargs)
+        super().__init__(A, B, C, (a, b, c), **kwargs)
 
 
-class GergonnePoint(Dot):
+class GergonnePoint(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (
+        super().__init__(A, B, C, (
             (b * c / (b + c - a)), (c * a / (c + a - b)), (a * b / (a + b - c))
-        )), **kwargs)
+        ), **kwargs)
 
 
-class NagelPoint(Dot):
+class NagelPoint(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (
+        super().__init__(A, B, C, (
             ((b + c - a) / a), ((c + a - b) / b), ((a + b - c) / c)
-        )), **kwargs)
+        ), **kwargs)
 
 
-class Mittenpunkt(Dot):
+class Mittenpunkt(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (
+        super().__init__(A, B, C, (
             (b + c - a), (c + a - b), (a + b - c)
-        )), **kwargs)
+        ), **kwargs)
 
 
-class SpiekerCenter(Dot):
+class SpiekerCenter(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         a, b, c = distance(B, C), distance(A, C), distance(A, B)
-        super().__init__(trilinear_to_cartesian(A, B, C, (
+        super().__init__(A, B, C, (
             (b * c * (b + c)), (a * c * (a + c)), (b * a * (b + a))
-        )), **kwargs)
+        ), **kwargs)
 
 
-class FeuerbachPoint(Dot):
+class FeuerbachPoint(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         angle_a = Angle.from_three_points(B, A, C).get_value()
         angle_b = Angle.from_three_points(A, B, C).get_value()
         angle_c = Angle.from_three_points(B, C, A).get_value()
-        super().__init__(trilinear_to_cartesian(A, B, C, (
+        super().__init__(A, B, C, (
             (1 - np.cos(angle_b - angle_c)), (1 - np.cos(angle_c - angle_a)), (1 - np.cos(angle_a - angle_b))
-        )), **kwargs)
+        ), **kwargs)
 
 
-class FermatPoint(Dot):
+class FermatPoint(TriangleCenter):
     def __init__(self, A, B, C, **kwargs):
         A, B, C = dot_to_array(A, B, C)
         angle_a = Angle.from_three_points(B, A, C).get_value()
         angle_b = Angle.from_three_points(A, B, C).get_value()
         angle_c = Angle.from_three_points(B, C, A).get_value()
         if angle_a > 2 / 3 * PI:
-            super().__init__(A)
-
+            super().__init__(A, B, C, (1, 0, 0), **kwargs)
         elif angle_b > 2 / 3 * PI:
-            super().__init__(B)
-
+            super().__init__(A, B, C, (0, 1, 0), **kwargs)
         elif angle_c > 2 / 3 * PI:
-            super().__init__(C)
-
-        super().__init__(trilinear_to_cartesian(A, B, C, (
-            csc(angle_a + PI / 3), csc(angle_b + PI / 3), csc(angle_c + PI / 3)
-        )), **kwargs)
+            super().__init__(A, B, C, (0, 0, 1), **kwargs)
+        else:
+            super().__init__(A, B, C, (
+                csc(angle_a + PI / 3), csc(angle_b + PI / 3), csc(angle_c + PI / 3)
+            ), **kwargs)
